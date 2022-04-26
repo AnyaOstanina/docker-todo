a194d82f52f7f11288cd5bc1ce3effcd67088da7
Anna Ostanina <aostanina@sinergo.ru>
2022-04-26 19:09:08 +0700

    Small fixes

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-BUILD_TIME=$(date +%s) && GIT_COMMIT=$(git log -1 --format=%h) && docker-compose build --build-arg GIT_COMMIT=$GIT_COMMIT --build-arg BUILD_TIME=$BUILD_TIME && docker-compose up -d
+GIT_COMMIT=$(git log -1 --format=%h) && BUILD_TIME=$(date +%s) && docker-compose build --build-arg GIT_COMMIT=$GIT_COMMIT --build-arg BUILD_TIME=$BUILD_TIME && docker-compose up -d
